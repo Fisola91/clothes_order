5c719b51589c64de92d00fb8f4eeb29cf7c9624d
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-05-13 09:35:14 -0700

    photo upload from cloudinary solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require "open-uri"
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,11 +6,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#file = https://www.istockphoto.com/photo/t-shirt-isolated-on-white-background-gm177412921-21296638
-Product.create!(name: "Blue T-shirt", description: "Blank blue wrinkled t-shirt mockup", price: 6.99)
 
-# file = https://www.istockphoto.com/photo/red-v-neck-shirt-design-template-gm1170500527-323921717
-Product.create!(name: "Red T-shirt", description: "Red v-neck t-shirt mock up", price: 6.99)
+Product.destroy_all
 
-#file = https://www.istockphoto.com/photo/blank-white-wrinkled-t-shirt-mockup-front-view-gm1307224792-397560626
-Product.create!(name: "Blank T-shirt", description: "Blank white wrinkled t-shirt mockup", price: 6.99)
+file_1 = URI.open("https://res.cloudinary.com/dolkzfrxl/image/upload/v1652434632/vswwaqnrx7vjvqpahczp.jpg")
+blue = Product.create!(name: "Blue T-shirt", description: "Blank blue wrinkled t-shirt mockup", price: 6.99)
+blue.photo.attach(io: file_1, filename: "cloth_1", content_type: 'image/jpg')
+
+file_2 = URI.open("https://res.cloudinary.com/dolkzfrxl/image/upload/v1652434672/cxtvlkn1xsmj2rfzlcfe.jpg")
+red = Product.create!(name: "Red T-shirt", description: "Red v-neck t-shirt mock up", price: 6.99)
+red.photo.attach(io: file_2, filename: "cloth_2", content_type: 'image/jpg')
+
+file_3 = URI.open("https://res.cloudinary.com/dolkzfrxl/image/upload/v1652434680/r69felb7zx6ofjbj4r1s.jpg")
+white = Product.create!(name: "Blank T-shirt", description: "Blank white wrinkled t-shirt mockup", price: 6.99)
+white.photo.attach(io: file_3, filename: "cloth_3", content_type: 'image/jpg')
