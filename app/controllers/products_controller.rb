653dc7843a5c0627f99c94ca14696791ac1a0e11
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-05-14 13:05:53 -0700

    implementing the number of visit count

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,21 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    @visit_count = 1
   end
+
+ private
+
+ def initialize_session
+  session[:visit_count] ||= 0 # This initialize the visit count on first visit
+ end
+
+ def increment_visit_count
+  session[:visit_count] += 1 # increment the count with each visit
+  @visit_count = session[:visit_count]
+ end
+
+
+
+
+
 end
