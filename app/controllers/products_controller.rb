b8c874afbe363b21bcee0b5b716fa5ca3c060567
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-05-30 08:23:22 +0200

    modified the index view and product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,31 +16,29 @@ class ProductsController < ApplicationController
 
 
   def load_cart
-    @count = session[:cart].count # This count number of product in the array
-
+    # This count number of product in the array
+    @count = session[:cart].count
     # When you give find an arrays of numbers, it gives back an aray of object of that type
     @cart = Product.find(session[:cart])
   end
 
-  # Delete item from the array when item is not needed
-  # To be implemented later
   def remove_from_cart
     id = params[:id].to_i
     session[:cart].delete(id)
     redirect_to root_path
   end
 
-
   private
 
-
   def initialize_session
-    session[:visit_count] ||= 0 # This initialize the visit count on first visit
+    # This initialize the visit count on first visit and cart
+    session[:visit_count] ||= 0
     session[:cart] ||= []
   end
 
   def increment_visit_count
-    session[:visit_count] += 1 # increment the count with each visit
+    # increment the count with each visit
+    session[:visit_count] += 1
     @visit_count = session[:visit_count]
   end
 end
